5262e36c4efde07742124f7884243d986359cb53
Jonas Nilsson <jonilss@gmail.com>
2017-08-21 09:50:20 +0200

    initial commit

---
+++ DagensCitat/DagensCitat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagensCitat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Glöm ej att skapa en branch "feature-förnamn" INNAN du gör ändringar i koden :)
+        }
+    }
+}
