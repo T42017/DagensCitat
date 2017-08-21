62754659460ed385f995e242ebb469c037f17426
pontuslaos <pontus.laos@lund.ntig.se>
2017-08-21 14:06:31 +0200

    Added DelayableQueue class. todo: implement way to tell the program at what time to print quote.

--- DagensCitat/DagensCitat/Program.cs
+++ DagensCitat/DagensCitat/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
 
 namespace DagensCitat
 {
     class Program
     {
-        private static string[] quotes = File.ReadAllLines(@"Resources\quotes.txt");
+        private static readonly string _filePath = Path.Combine(Environment.CurrentDirectory, @"..\..", @"Resources\quotes.txt");
+        private static DelayableQueue<string> _quotes = new DelayableQueue<string>(File.ReadAllLines(_filePath));
+        private static TimeSpan _timeOfDayToStart = TimeSpan.Parse("13:56");
+        private static TimeSpan _timeOfDayToEnd = TimeSpan.Parse("13:58");
 
         static void Main(string[] args)
         {
-            var builder = new StringBuilder();
-            foreach (var quote in quotes)
+            while (true)
             {
-                builder.Append(quote + "\r\n");
+                var quote = _quotes.DequeueAndEnqueueWithDelay(TimeSpan.FromMilliseconds(250));
+                Console.WriteLine(quote);
             }
-            Console.WriteLine(builder.ToString());
         }
     }
 }
