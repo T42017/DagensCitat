924ab17325255452aaa1559a3eb4e2071d87deef
Rasmus Harrysson <rasmus.harrysson@lund.ntig.se>
2017-08-21 13:48:09 +0200

    Nu helt klar.

--- DagensCitat/DagensCitat/Program.cs
+++ DagensCitat/DagensCitat/Program.cs
@@ -12,20 +12,8 @@ namespace DagensCitat
     {
         static void Main(string[] args)
         {
-            List<string> quotes = new List<string>();
-
-            string path = @"C:\Git\DagensCitat-Uppgift\DagensCitat\DagensCitat\DagensCitat\Citat.txt";
-            using (FileStream fs = new FileStream(path, FileMode.Open))
-            {
-                using (StreamReader sr = new StreamReader(fs))
-                {
-
-                    while (sr.Peek() >= 0)
-                    {
-                        quotes.Add(sr.ReadLine());
-                    }
-                }
-            }
+            List<string> quotes = System.IO.File
+                .ReadAllLines(@"C:\Git\DagensCitat-Uppgift\DagensCitat\DagensCitat\DagensCitat\Citat.txt").ToList();      
 
             int day = DateTime.Today.DayOfYear;            
             int quoteIndex = day % quotes.Count; 
