780fed1c9b3e87066798c003dbc962203896dd9c
pontuslaos <pontus.laos@lund.ntig.se>
2017-08-21 11:24:03 +0200

    First commit. Attempted to add quotes.

--- DagensCitat/DagensCitat/Program.cs
+++ DagensCitat/DagensCitat/Program.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DagensCitat
 {
     class Program
     {
+        private static string[] quotes = File.ReadAllLines(@"Resources\quotes.txt");
+
         static void Main(string[] args)
         {
-            //Glöm ej att skapa en branch "feature-förnamn" INNAN du gör ändringar i koden :)
+            var builder = new StringBuilder();
+            foreach (var quote in quotes)
+            {
+                builder.Append(quote + "\r\n");
+            }
+            Console.WriteLine(builder.ToString());
         }
     }
 }
