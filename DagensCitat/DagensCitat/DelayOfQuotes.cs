5ad1855fa2d696295235b62e86a828bac7439aff
Filip Wall <filip.wall@lund.ntig.se>
2017-08-21 14:09:16 +0200

    Added a feature that adds a new quote after 24 hours

---
+++ DagensCitat/DagensCitat/DelayOfQuotes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DagensCitat
+{
+    class DelayOfQuotes<Temp>
+    {
+        private readonly List<Temp> _quotes;
+
+        public DelayOfQuotes()
+        {
+            _quotes = new List<Temp>();
+
+        }
+
+        public DelayOfQuotes(Temp[] input) : this()
+        {
+            foreach (var inputItem in input)
+            {
+                _quotes.Add(inputItem);
+            }
+        }
+
+        public void Enqueue(Temp quote)
+        {
+            _quotes.Add(quote);
+        }
+
+        public Temp Dequeue()
+        {
+            return EnqueueTheDequeue(TimeSpan.Zero);
+        }
+
+        public Temp EnqueueTheDequeue(TimeSpan delayTime)
+        {
+            if (_quotes.Count == 0)
+            {
+                throw new NullReferenceException();
+            }
+
+            Thread.Sleep(delayTime);
+
+            var firstQuote = _quotes[0];
+            _quotes.RemoveAt(0);
+            _quotes.Add(firstQuote);
+            return firstQuote;
+        }
+
+    }
+}
--- DagensCitat/DagensCitat/Program.cs
+++ DagensCitat/DagensCitat/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace DagensCitat
 {
     class Program
     {
+        static readonly string FilePath = (Environment.CurrentDirectory + @"\..\..\Properties\txt_daily.txt");
+        static DelayOfQuotes<string> _quotes = new DelayOfQuotes<string>(File.ReadAllLines(FilePath));
+
+
         static void Main(string[] args)
         {
-            //Glöm ej att skapa en branch "feature-förnamn" INNAN du gör ändringar i koden :)
+            while (true)
+            {
+                var quote = _quotes.EnqueueTheDequeue(TimeSpan.FromHours(24));
+                Console.WriteLine(quote);
+            }
         }
     }
 }
