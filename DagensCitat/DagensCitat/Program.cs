0c738d5998327000f73fe54230a5b7b0351e7cbb
Simon Svensson <simon.svensson@lund.ntig.se>
2017-08-21 14:06:00 +0200

    Working example

--- DagensCitat/DagensCitat/Program.cs
+++ DagensCitat/DagensCitat/Program.cs
@@ -8,9 +8,21 @@ namespace DagensCitat
 {
     class Program
     {
+        private static readonly DateTime StartDate = new DateTime(2017, 8, 21);
+
         static void Main(string[] args)
         {
-            //Glöm ej att skapa en branch "feature-förnamn" INNAN du gör ändringar i koden :)
+            List<String> quates = new List<String>();
+            quates.Add("I love you the more in that I believe you had liked me for my own sake and for nothing else. John Keats");
+            quates.Add("But man is not made for defeat. A man can be destroyed but not defeated. Ernest Hemingway");
+            quates.Add("When you reach the end of your rope, tie a knot in it and hang on. Franklin D.Roosevelt");
+            quates.Add("There is nothing permanent except change. Heraclitus");
+
+            DateTime thisDate = DateTime.Now;
+            int daysFromStartDate = (int) (thisDate - StartDate).TotalDays;
+            String quate = quates.ElementAt(daysFromStartDate % quates.Count);
+            Console.WriteLine(quate);
+            Console.ReadLine();
         }
     }
 }
