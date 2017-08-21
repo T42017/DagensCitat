a34a8337a0fc4f72d5b1ec536eb89ad2fcd14f6d
Niklas Lindblom-Hertz <niklas.lindblomhertz@lund.ntig.se>
2017-08-21 14:01:53 +0200

    Feature with quotes (working)

--- DagensCitat/DagensCitat/Program.cs
+++ DagensCitat/DagensCitat/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DagensCitat
 {
     class Program
@@ -11,7 +12,15 @@ namespace DagensCitat
         static void Main(string[] args)
         {
             //Glöm ej att skapa en branch "feature-förnamn" INNAN du gör ändringar i koden :)
-            Console.WriteLine("hej");
+            string currentDirName = System.IO.Directory.GetCurrentDirectory();
+            List<string> quotes = System.IO.File.ReadAllLines(currentDirName+ @"\citat.txt").ToList();
+            int dayOfYear = int.Parse(DateTime.Today.DayOfYear.ToString());
+            int quoteChecker = dayOfYear % quotes.Count;
+            Console.WriteLine("Quote of the day is: " + quotes[quoteChecker]);
+            Console.ReadLine();
+
+
+
         }
     }
 }
