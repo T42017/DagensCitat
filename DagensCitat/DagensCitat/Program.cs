7f516f2e74189afcfca72653ce36d1fed6d76d85
Mathias Andersson <mathias.andersson@lund.ntig.se>
2017-08-22 10:10:54 +0200

    Second time im done.

--- DagensCitat/DagensCitat/Program.cs
+++ DagensCitat/DagensCitat/Program.cs
@@ -11,6 +11,37 @@ namespace DagensCitat
         static void Main(string[] args)
         {
             //Glöm ej att skapa en branch "feature-förnamn" INNAN du gör ändringar i koden :)
+
+            List<string> quoteList = new List<string>();
+            quoteList.Add("1");
+            quoteList.Add("2");
+            quoteList.Add("3");
+            quoteList.Add("4");
+            quoteList.Add("5");
+            quoteList.Add("6");
+            quoteList.Add("7");
+            quoteList.Add("8");
+            quoteList.Add("9");
+            quoteList.Add("10");
+
+
+
+
+
+            int numberOfQuotes = quoteList.Count;
+
+            DateTime dateThen = new DateTime(2017, 08, 22, 00, 00, 00);
+            DateTime dateNow = DateTime.Now;
+            double time = dateNow.Subtract(dateThen).TotalDays;
+
+            while (time > numberOfQuotes)
+            {
+                dateThen = dateThen.AddDays(numberOfQuotes);
+                time = dateNow.Subtract(dateThen).TotalDays;
+            }
+
+            Console.WriteLine(quoteList[Convert.ToInt32(time)]);
+
         }
     }
 }
